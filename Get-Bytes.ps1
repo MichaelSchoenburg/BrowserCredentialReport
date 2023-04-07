df08d6f4c503e5e6a127d75f083071a8a5a42900
MichaelSchoenburg <michaelschoenburg+git@gmail.com>
2023-04-07 23:11:44 +0200

    added comments and requirements

--- Get-Bytes.ps1
+++ Get-Bytes.ps1
@@ -1,13 +1,15 @@
+#Requires -Version 5
+
 # Download the tools from here (search for "1. Click this download link."): https://www.nirsoft.net/password_recovery_tools.html
 
 $Path = "$($env:USERPROFILE)\Downloads" # Paste Path to the downloaded tools here. Both tools should be in the same folder.
 
-$Content = Get-Content -Path "$($Path)\WebBrowserPassView.exe" -Encoding Byte
+$Content = Get-Content -Path "$($Path)\WebBrowserPassView.exe" -Encoding Byte  # Byte encoding exists in PS5 only
 $Base64 = [System.Convert]::ToBase64String($Content)
 $Base64 | Clip # Will copy the Byte64 string into your clipboard so you can paste it into the other script inside the variable Base64WebBrowserPassView
 
 Pause
 
-$Content = Get-Content -Path "$($Path)\PasswordFox.exe" -Encoding Byte
+$Content = Get-Content -Path "$($Path)\PasswordFox.exe" -Encoding Byte # Byte encoding exists in PS5 only
 $Base64 = [System.Convert]::ToBase64String($Content)
 $Base64 | Clip # Will copy the Byte64 string into your clipboard so you can paste it into the other script inside the variable ContentPasswordFox
